54085af3ab363b391c397f0fd8443938881558fc
Somnath Pandit <panditsomnath10016@gmail.com>
2020-11-23 00:12:44 +0530

    First upload

---
+++ complex_alg.py
@@ -0,0 +1,90 @@
+from manimlib.imports import *
+
+class PlottingGraphs(GraphScene):
+	CONFIG = {
+	"x_min": -2,
+	"x_max": 10,
+	"y_min": -2,
+	"y_max": 10,
+	"graph_origin": ORIGIN+2*(2*LEFT+DOWN),
+	"x_axis_label":"$Re(z)$",
+	"y_axis_label":"$Im(z)$",
+	"x_labeled_nums": list(np.arange(-2, 11,2)),
+	"y_labeled_nums": list(np.arange(-2, 11,2)),
+	"x_axis_width": 10,
+	"y_axis_height": 6,
+	}
+	def construct(self):
+		XD = self.x_axis_width/(self.x_max- self.x_min)
+		YD = self.y_axis_height/(self.y_max- self.y_min)
+	
+		title=TextMobject(r"Seeing the Addition of two Complex Numbers \\ in Complex Plane")
+
+		X=XD*RIGHT ;Y=YD*UP
+
+		a1=4*X+4*Y
+		a2=2*X+5*Y
+		a=a1+a2
+
+		v1=Vector(direction=a1).set_color(BLUE)
+		v1.shift(self.graph_origin)
+		v2=Vector(direction=a2).set_color(YELLOW)
+		v2.shift(self.graph_origin)
+		v=Vector(direction=a).set_color(GREEN)
+		v.shift(self.graph_origin)
+
+		v1_lx = (DashedLine(ORIGIN,4*X)).set_color(BLUE)
+		v1_ly = (DashedLine(ORIGIN,4*Y)).set_color(BLUE)
+		v1_lx.shift(v1.get_corner(LEFT+UP))
+		v1_ly.shift(v1.get_corner(RIGHT+DOWN))
+
+		v2_lx = (DashedLine(ORIGIN,2*X)).set_color(YELLOW)
+		v2_ly = (DashedLine(ORIGIN,5*Y)).set_color(YELLOW)
+		v2_lx.shift(v2.get_corner(LEFT+UP))
+		v2_ly.shift(v2.get_corner(RIGHT+DOWN))
+
+		v_lx = (DashedLine(ORIGIN,6*X)).set_color(GREEN)
+		v_ly = (DashedLine(ORIGIN,9*Y)).set_color(GREEN)
+		v_lx.shift(v.get_corner(LEFT+UP))
+		v_ly.shift(v.get_corner(RIGHT+DOWN))
+
+		v1_text=TextMobject(r"$z_1=4+4i$")
+		v2_text=TextMobject(r"$z_2=2+5i$")
+		v_text=TextMobject(r"$z=z_1+z_2$")
+		v_text1=TextMobject(r"$=6+9i$").scale(.8)
+		v1_text=(v1_text.next_to(v1)).scale(.8)
+		v2_text=(v2_text.next_to(v2)).scale(.8)
+		v_text=v_text.shift(1.3*v.get_corner(UP+RIGHT)).scale(.8)
+		v_text1=v_text1.next_to(v_text,DOWN)
+		
+		z_2=VGroup(*[v2, v2_text,v2_lx,v2_ly ])
+
+		self.play(ShowCreation(title)) 
+		self.wait(2)
+		self.play(FadeOut(title))
+
+		self.setup_axes(animate = True)
+		self.wait(.5)
+
+		self.play(ShowCreation(v1))
+		self.play(ShowCreation(v1_text),ShowCreation(v1_lx),ShowCreation(v1_ly))
+		self.wait(.5)
+
+		self.play(ShowCreation(v2))
+		self.play(ShowCreation(v2_text),ShowCreation(v2_lx),ShowCreation(v2_ly))
+		self.wait(.5)
+		
+		self.play(ApplyMethod(z_2.shift,a1))
+		self.play(ApplyMethod(v1_lx.shift,5*Y) , ApplyMethod(v1_ly.shift,2*X), ApplyMethod(v1_text.shift,2*X))
+		self.wait(1)
+
+		self.play(ShowCreation(v))
+		self.play(ShowCreation(v_text))
+		self.play(ShowCreation(v_text1),Transform(VGroup(v1_lx,v2_lx),v_lx), Transform(VGroup(v1_ly,v2_ly),v_ly))
+		self.wait(3)
+
+
+
+
+
+
---
+++ fourier_terms.py
@@ -0,0 +1,68 @@
+from manimlib.imports import *
+
+class PlottingGraphs(GraphScene):
+	CONFIG = {
+	"x_min": -4,
+	"x_max": 4,
+	"y_min": -2,
+	"y_max": 2,
+	"graph_origin": ORIGIN,
+	"x_labeled_nums": list(range(-4, 5)),
+	"y_labeled_nums": list(range(-2, 3)),
+	"x_axis_width": 12,
+	"y_axis_height": 4,
+	"total_fourier_terms": 21,
+	}
+	def construct(self):
+		XTD = self.x_axis_width/(self.x_max- self.x_min)
+		YTD = self.y_axis_height/(self.y_max- self.y_min)
+		
+#		sq_wave=self.get_graph(self.sqwave)
+		title1=TextMobject(r"Any Periodic function ($f(x)$) with period L obeying Dirichlet's \\ Condition can be represented by Fourier Series $--$").to_edge(2*UP)
+		title2=TextMobject(r" $f(x)=\sum_{n=0}^{\infty}(a_n Cos(\frac{2\pi nx}{L})+b_n Sin(\frac{2\pi nx}{L}))$ ").set_color(GOLD).next_to(title1,2*DOWN)
+		title3=TextMobject(r"Let's see how increasing number of terms in Fourier Series \\ can approximate a Square Wave function ").next_to(title2,4*DOWN)
+		self.play(ShowCreation(title1)) ;self.wait(3)
+		self.play(ShowCreation(title2)) ;self.wait(2)
+		self.play(ShowCreation(title3)) ;self.wait(5)
+		self.play(FadeOut(title1),FadeOut(title2))
+		
+		term_text=(TextMobject(r"No of fourier Terms \large{n} $=$")).to_corner(UL)
+		self.play(ReplacementTransform(title3,term_text))
+		
+		self.setup_axes(animate = True)
+		
+#Plotting the fourier terms using loop	
+#		self.bn=np.array([ 0, 0.63661977, -0.31830989,  0.21220659 ,-0.15915494 , 0.12732395, -0.1061033, 0.09094568 ,-0.07957747,  0.07073553 ,-0.06366198 , 0.05787452, -0.05305165,0.04897075 ,-0.04547284 , 0.04244132, -0.03978874 , 0.03744822,-0.03536777,0.0335063  ,-0.03183099]) #Data for sawtooth wave
+
+		self.bn=np.array([ 0,1.27323954e+00 , 7.96888597e-18 , 4.24413182e-01 , 3.37864502e-17 ,2.54647909e-01,  3.20313979e-17 , 1.81891364e-01 ,-5.35324823e-18, 1.41471061e-01, -1.92852461e-17  ,1.15749050e-01 , 7.59280334e-18, 9.79415034e-02  ,6.84402621e-18,  8.48826363e-02, -4.13690891e-18, 7.48964438e-02,  4.66037528e-17 , 6.70126076e-02 ,-1.54837623e-18]) #coefficients collected form a separete fourier program for square wave 
+			
+		for N in range(self.total_fourier_terms):	
+		 if self.bn[N]>1e-5 or N==0:			#excluding terms having small coefficient value
+		  self.fourier_term_no=N
+		  term_num=TextMobject("$%d$"%N).next_to(term_text,RIGHT)
+		  get_func=self.get_graph(self.sin_terms).set_color(BLUE)
+		  
+		  if N>0:
+		  	self.play(ReplacementTransform (previous_func,get_func), ReplacementTransform(previous_term,term_num))
+		  else:
+		  	self.play(ShowCreation(get_func),ShowCreation(term_num))
+		  print (N)
+		  previous_func=get_func
+		  previous_term=term_num
+		  self.wait(1.4)
+		  
+#	def sqwave(self,x):
+#		period =8.0
+#		if ((self.x_max - x)%(period/2.0))<1:
+#			return 1
+#		else :
+#			return -1
+			
+	def sin_terms(self,x):
+		l=4			#periodicity of the function
+		func=0.0	#a0 term
+		for n in range(1,1+self.fourier_term_no):
+		  func=func+(self.bn[n]*np.sin(2*PI*n*x/l))
+		return func
+
+
